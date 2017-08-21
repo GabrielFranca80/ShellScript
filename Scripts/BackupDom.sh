be5c088d435751b3f8f8b450ca94c46c3adabc43
noir <gabriel.franca@gmail.com>
2017-08-21 17:04:04 -0300

    Corrigi o bkp do share e add comentários para facilitar leitura

--- Scripts/BackupDom.sh
+++ Scripts/BackupDom.sh
@@ -27,12 +27,13 @@ user="/dados/users"
 share="/dados/share/"
 dpto="/dados/dpto"
 
-
 echo "log dos backups"
 dir_log=/var/log/backup
 
 ######################### Funções ###########################
 
+## Função de limpeza vai deletar os arquivos com base na quantidade de dias selecionado para retenção. ##
+	
 function LIMPABACKUP () {
 				#Diretorio da Limpeza
 						diretorio_backup="/media/smart/backup"
@@ -40,7 +41,7 @@ function LIMPABACKUP () {
 				#Arquivo de Log
 						log="/var/log/backup/limpeza/data.log";
 
-				#Remove os arquivos que foram movidos para o backup com a quantidade de dias escolhido abaixo.
+				#Define a quantidade de dias a serem mantidos.
 						dias="10";
 
 				#Remove os arquivos antigos baseados na data em que foram movidos para lixeira
@@ -58,6 +59,8 @@ function LIMPABACKUP () {
 
 echo "Função do backup da pasta Home"
 
+## Função para backup da pasta dos usuários (pasta individual) ##
+
 function BACKUPHOME () {
 
                 for home in $user/*; do
@@ -73,6 +76,8 @@ function BACKUPHOME () {
                 
 echo " Função do backup da pasta Dpto"
 
+## Função para backup da pasta departamental ##
+
 function BACKUPDPTO () {
 
             	for dpt in $dpto/*; do
@@ -86,18 +91,30 @@ function BACKUPDPTO () {
 
                 } 
 
+## Função para backup da pasta compartilhada nessa pasta os usuários podem criar e deletar qualquer tipo de arquivo ##
+
 function BACKUPSHARE () {
+	## Efetuando Backup dos arquivos soltos dentro da pasta Share ##
+	ARQ=`find $share -maxdepth 1 -type f | awk -F / {'print $NF'}`
+						cd /dados/share/
+						tar -czvf $dir_temp/sharearq-$data.tgz $ARQ 
+						mv $dir_temp/sharearq-$data.tgz $disp_backup/$data/share/
+	## Fim da parte que realiza backup dos arquivos Soltos na pasta Share ##
+						
+	## Inicio do Backup individual das pastas dentro do share ## 
 	IFS="^M"
 	find $share -maxdepth 1 -type d |awk -F / {'print $NF'} | sed "1d" | sed 's/ /\\ /g' > /tmp/ARQ.txt
 						
 				while read I; do 
-						tar -czvf $dir_temp/${I}.tgz $share/${I}
+						tar -czvf $dir_temp/${I}-$data.tgz $share/${I}
                    		echo "Movendo o Backup para o SMART"
-						mv $dir_temp/$LOCAL-$data.tgz $disp_backup/$data/dpto/;
+						mv $dir_temp/${I}-$data.tgz $disp_backup/$data/share/;
 				done </tmp/ARQ.txt
-
-                }
-                
+				reset		
+	## Fim do backup do Share ##			
+	            }
+## Função para realizar o backup das pastas de configuração do servidor linux definidos lá no inicio do programa ##
+	                
 function BACKUPLX () {
 
                 tar -czvf $arquivo_temp1 $lx --exclude-from=/root/gerencia/scripts/bkpexclude > $arquivo_log1
@@ -105,13 +122,17 @@ function BACKUPLX () {
                 
                                            
 ######################### Montagem do HD externo e inicio do Backup ###########################  
-#Desmontando o hd externo caso ele esteja montado
-	umount /media/smart
+
+# Desmontando o hd externo caso ele esteja montado
+
+umount /media/smart
 sleep 2
-##mount /dev/sdb1 /media/smart
-#Montando o SMART e validando a montagem
+
+## mount /dev/sdb1 /media/smart
+## Montando o SMART e validando a montagem coloquei um teste para caso o hd externo não esteja presente o sistema envie um email com a mensagem.
+## Veja que estou usando um LABEL NO HD EXTERNO faço isso para evitar que outro dispositivo use o caminho e acabe dando erro. 	
 if ! mount LABEL=hdbkp /media/smart; then
-   echo "Subject:07 - Erro na unidade de Backup da HB Engenharia" > $arquivo_log
+   echo "Subject:07 - Erro na unidade de Backup" > $arquivo_log
    echo "hd externo com erro backup nao realizado !!!!!" >> $arquivo_log
         SHELL=/bin/sh
         PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
@@ -138,12 +159,17 @@ sleep 5
 ######################### Relatório do Backup enviado por e-mail ###########################
         
 #Testar arquivo gzip
+
 #gzip -tv backup.tar.gz 
+
 #backup.tar.gz:   OK asdf
 
 
 
 ######################### Teste de descompressão dos arquivos guardados no HD externo ###########################        
+
+
+
 ######################### Ver espaço em disco e envio do email ###########################	
     	
 #   	 echo -e "`df -h`" >> $arquivo_log
@@ -160,15 +186,15 @@ sleep 5
 #        echo "Fim do backup as $hora1" >> $arquivo_log
 #        echo "Enviando email"
 #        ssmtp -C /root/gerencia/scripts/ssmtp.conf bkpgrp@cmcsolucoes.com.br < $arquivo_log
-#fi
+fi
 
-#sync 
-#sleep 20
-#sync
+sync 
+sleep 20
+sync
 ######################### Desmontar as unidades externas ###########################	
 
-#echo "Desmotando o SMART"
-#umount /media/smart
+echo "Desmontando o SMART"
+umount /media/smart
 
 ######################### Fim do backup e tempo de duração ###########################	
 echo 
